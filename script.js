8f1a472b3592288f3cdab1132a5b265f37b61e3d
rickvieira192 <rickvieira192@gmail.com>
2021-04-16 10:30:39 -0300

    criar comidinha

--- script.js
+++ script.js
@@ -8,7 +8,12 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
 
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+
+}
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -25,6 +30,14 @@ function criarCobrinha(){
     }
 
 }
+
+
+function drawFood(){
+
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 
 document.addEventListener('keydown', update);
 
@@ -46,6 +59,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
